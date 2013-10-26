270bfdaebd22f89f8d985c785a13c9ab100663bd
sk.kim <jesuskim16@gmail.com>
2013-10-26 18:27:16 +0900

    Spring Test Project Upload

---
+++ src/main/java/com/pam/test/springtest/MainController.java
@@ -0,0 +1,26 @@
+package com.pam.test.springtest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/pam")
+public class MainController {
+	
+	private final Logger logger = LoggerFactory.getLogger(MainController.class);
+	
+	@RequestMapping(value = "/test" ,  method = RequestMethod.GET)
+	@ResponseBody
+	public  String mainController(){
+		
+		String returnValue = "Hello World";
+		logger.info("{}" , returnValue);
+		
+		return returnValue;
+	}
+	
+}
